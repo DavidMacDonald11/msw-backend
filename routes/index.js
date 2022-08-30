b34fdb890ae4a24cff5f39b1ffa08f082a1b3041
david <davidjmacdonald11@gmail.com>
2022-08-30 18:36:08 -0400

    Add richer test output

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,10 @@ import express from "express"
 
 const router = express.Router()
 
-router.get("*", (req, res) => {
-    res.send("Backend")
-});
+router.get("/api/:which", (req, res) => {
+    res.send(JSON.stringify({
+        message: `Backend at ${req.params.which}`
+    }))
+})
 
 export default router
